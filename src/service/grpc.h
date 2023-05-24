d84d9f1b906b2a9a06f210b338601b60dcd4e209
Uditha Atukorala <ua@nuked.zone>
2023-05-25 00:59:15 +0100

    (service) cleanup includes

--- src/service/grpc.h
+++ src/service/grpc.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "protos/gk/v1/gatekeeper.grpc.pb.h"
+#include "gk/v1/gatekeeper.grpc.pb.h"
 
 namespace service {
 class Grpc final : public gk::v1::Gatekeeper::CallbackService {};
