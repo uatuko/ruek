7cd37b370f079992de69bcc785851367edf5ef2b
Uditha Atukorala <ua@nuked.zone>
2023-05-31 23:20:04 +0100

    (service) create identity implementation

--- src/service/grpc.cpp
+++ src/service/grpc.cpp
@@ -1,12 +1,25 @@
 #include "grpc.h"
 
+#include "mappers.h"
+
 namespace service {
 grpc::ServerUnaryReactor *Grpc::CreateIdentity(
 	grpc::CallbackServerContext *context, const gk::v1::CreateIdentityRequest *request,
 	gk::v1::Identity *response) {
 	auto *reactor = context->DefaultReactor();
 
-	reactor->Finish(grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "Not implemented."));
+	auto identity = map(request);
+	try {
+		identity.store();
+	} catch (...) {
+		// FIXME: capture errors
+		reactor->Finish(grpc::Status(grpc::StatusCode::UNAVAILABLE, "Failed to store data."));
+		return reactor;
+	}
+
+	map(identity, response);
+
+	reactor->Finish(grpc::Status::OK);
 	return reactor;
 }
 } // namespace service
