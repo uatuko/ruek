bbe5b64cdbe9d309e7c4a659aa9ae6d5aa2125ed
Uditha Atukorala <ua@nuked.zone>
2023-06-02 20:38:56 +0100

    (service) create collection

--- src/service/grpc.h
+++ src/service/grpc.h
@@ -5,6 +5,10 @@
 namespace service {
 class Grpc final : public gk::v1::Gatekeeper::CallbackService {
 public:
+	grpc::ServerUnaryReactor *CreateCollection(
+		grpc::CallbackServerContext *context, const gk::v1::CreateCollectionRequest *request,
+		gk::v1::Collection *response) override;
+
 	grpc::ServerUnaryReactor *CreateIdentity(
 		grpc::CallbackServerContext *context, const gk::v1::CreateIdentityRequest *request,
 		gk::v1::Identity *response) override;
--- src/service/mappers.h
+++ src/service/mappers.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "datastore/collections.h"
 #include "datastore/identities.h"
 #include "gk/v1/gatekeeper.grpc.pb.h"
 
 namespace service {
-datastore::Identity map(const gk::v1::CreateIdentityRequest *from);
+datastore::Collection map(const gk::v1::CreateCollectionRequest *from);
+datastore::Identity   map(const gk::v1::CreateIdentityRequest *from);
 
+void map(const datastore::Collection &from, gk::v1::Collection *to);
 void map(const datastore::Identity &from, gk::v1::Identity *to);
 } // namespace service
