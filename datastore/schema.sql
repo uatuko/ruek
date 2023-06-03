68a3106ca8ae091b5cc554dd8f963869b8e1de14
Uditha Atukorala <ua@nuked.zone>
2023-06-03 11:07:00 +0100

    tidy-up schema

--- datastore/schema.sql
+++ datastore/schema.sql
@@ -3,7 +3,8 @@ create table if not exists collections (
 	_rev integer not null,
 	name text    not null,
 
-	constraint "collections.pkey" primary key (_id)
+	constraint "collections.pkey" primary key (_id),
+	constraint "collections.check-name" check (name <> '')
 );
 
 create table if not exists identities (
@@ -12,12 +13,13 @@ create table if not exists identities (
 	sub  text    not null,
 
 	constraint "identities.pkey" primary key (_id),
-	constraint "identities.key-sub" unique (sub)
+	constraint "identities.key-sub" unique (sub),
+	constraint "identities.check-sub" check (sub <> '')
 );
 
 create table if not exists collection_members (
-	collection_id text,
-	identity_id   text,
+	collection_id text not null,
+	identity_id   text not null,
 
 	constraint "collection_members.pkey" primary key (collection_id, identity_id),
 	constraint "collection_members.fkey-collection_id" foreign key (collection_id)
