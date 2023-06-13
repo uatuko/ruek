1622e704352c9bae82d1cf69be69509ec7a73010
Laura Necula <neculalaura@gmail.com>
2023-06-13 20:55:46 +0100

    (datastore) define DB schema

--- datastore/schema.sql
+++ datastore/schema.sql
@@ -1,3 +1,11 @@
+create table if not exists acl-policies (
+	_id   text    not null,
+	_rev  integer not null,
+	rules jsonb,
+
+	constraint "rbac-policies.pkey" primary key (_id)
+);
+
 create table if not exists collections (
 	_id  text    not null,
 	_rev integer not null,
@@ -17,7 +25,38 @@ create table if not exists identities (
 	constraint "identities.check-sub" check (sub <> '')
 );
 
-create table if not exists collection_members (
+create table if not exists rbac-policies (
+	_id   text    not null,
+	_rev  integer not null,
+	name  text    not null,
+	rules jsonb,
+
+	constraint "rbac-policies.pkey" primary key (_id)
+);
+
+create table if not exists roles (
+	_id         text    not null,
+	_rev        integer not null,
+	name        text    not null,
+	permissions text[],
+
+	constraint "roles.pkey" primary key (_id)
+);
+
+create table if not exists collections_access-policies (
+	collection_id text not null,
+	policy_id     text not null,
+
+	constraint "collections_access-policies.pkey" primary key (identity_id, policy_id),
+	constraint "collections_access-policies.fkey-identity_id" foreign key (collection_id)
+		references collections(_id)
+		on delete cascade
+	constraint "collections_access-policies.fkey-policy_id" foreign key (policy_id)
+		references access-policies(_id)
+		on delete cascade,
+);
+
+create table if not exists collections_identities (
 	collection_id text not null,
 	identity_id   text not null,
 
@@ -27,5 +66,57 @@ create table if not exists collection_members (
 		on delete cascade,
 	constraint "collection_members.fkey-identity_id" foreign key (identity_id)
 		references identities(_id)
+		on delete cascade
+);
+
+create table if not exists collections_rbac-policies (
+	collection_id text not null,
+	policy_id     text not null,
+
+	constraint "collections_rbac-policies.pkey" primary key (identity_id, policy_id),
+	constraint "collections_rbac-policies.fkey-identity_id" foreign key (collection_id)
+		references collections(_id)
+		on delete cascade
+	constraint "collections_rbac-policies.fkey-policy_id" foreign key (policy_id)
+		references rbac-policies(_id)
+		on delete cascade,
+);
+
+create table if not exists identities_access-policies (
+	identity_id text not null,
+	policy_id   text not null,
+
+	constraint "identities_access-policies.pkey" primary key (identity_id, policy_id),
+	constraint "identities_access-policies.fkey-identity_id" foreign key (identity_id)
+		references identities(_id)
+		on delete cascade
+	constraint "identities_access-policies.fkey-policy_id" foreign key (policy_id)
+		references access-policies(_id)
+		on delete cascade,
+);
+
+create table if not exists identities_rbac-policies (
+	identity_id text not null,
+	policy_id   text not null,
+
+	constraint "identities_rbac-policies.pkey" primary key (identity_id, policy_id),
+	constraint "identities_rbac-policies.fkey-identity_id" foreign key (identity_id)
+		references identities(_id)
+		on delete cascade
+	constraint "identities_rbac-policies.fkey-policy_id" foreign key (policy_id)
+		references rbac-policies(_id)
+		on delete cascade,
+);
+
+create table if not exists rbac-policies_roles (
+	policy_id text not null,
+	role_id   text not null,
+
+	constraint "rbac-policies_roles.pkey" primary key (policy_id, role_id),
+	constraint "rbac-policies_roles.fkey-policy_id" foreign key (policy_id)
+		references rbac-policies(_id)
+		on delete cascade,
+	constraint "rbac-policies_roles.fkey-role_id" foreign key (role_id)
+		references roles(_id)
 		on delete cascade
 );
