338199612b1eaff6f9011c4b4f56b73c46a9680e
Uditha Atukorala <ua@nuked.zone>
2023-06-25 06:07:03 +0100

    (events) include timestamp when serialising

--- src/events/basic_event.h
+++ src/events/basic_event.h
@@ -9,15 +9,43 @@
 
 namespace events {
 template <encodable T> struct basic_event {
-	using payload_t   = T;
-	using timestamp_t = std::chrono::sys_time<std::chrono::nanoseconds>;
+	using clock_t   = std::chrono::system_clock;
+	using payload_t = T;
+
+	struct timestamp_t : public clock_t::time_point {
+		timestamp_t(const clock_t::time_point &tp) : clock_t::time_point(tp) {}
+		timestamp_t(clock_t::time_point &&tp) : clock_t::time_point(std::move(tp)) {}
+
+		std::string rfc3339nano() const {
+			const auto                  tp   = *this;
+			auto                        days = std::chrono::floor<std::chrono::days>(tp);
+			std::chrono::year_month_day ymd(days);
+			std::chrono::hh_mm_ss time(std::chrono::floor<std::chrono::nanoseconds>(tp - days));
+
+			char buf[std::size("2006-01-02T15:04:05.999999999Z")];
+			std::snprintf(
+				std::data(buf),
+				std::size(buf),
+				"%d-%.2u-%.2uT%.2ld:%.2ld:%.2lld.%lldZ",
+				static_cast<int>(ymd.year()),
+				static_cast<unsigned>(ymd.month()),
+				static_cast<unsigned>(ymd.day()),
+				time.hours().count(),
+				time.minutes().count(),
+				time.seconds().count(),
+				time.subseconds().count());
+
+			return buf;
+		}
+	};
 
 	std::string name;
 	payload_t   payload;
-	timestamp_t timestamp;
+	timestamp_t timestamp = std::chrono::system_clock::now();
 
 	std::string serialize() const {
-		auto obj = glz::obj{"name", name, "payload", payload.encode()};
+		auto obj = glz::obj{
+			"name", name, "payload", payload.encode(), "timestamp", timestamp.rfc3339nano()};
 
 		return glz::write_json(obj);
 	}
--- src/events/events_test.cpp
+++ src/events/events_test.cpp
@@ -27,13 +27,17 @@ TEST(events, publish) {
 		auto &p = events::publisher();
 		p.transport(transport);
 
+		std::chrono::microseconds                          us(1'687'668'087'461'048);
+		std::chrono::time_point<std::chrono::system_clock> tp(us);
+
 		events::basic_event<payload> e{
-			.name = "name:events.publish",
+			.name      = "name:events.publish",
+			.timestamp = tp,
 		};
 		events::publish(e);
 
 		std::string expected =
-			R"({"name":"name:events.publish","payload":{"type":"test","data":{"bool":false,"int":45,"str":"string of data"}}})";
+			R"({"name":"name:events.publish","payload":{"type":"test","data":{"bool":false,"int":45,"str":"string of data"}},"timestamp":"2023-06-25T04:41:27.461048000Z"})";
 		EXPECT_EQ(expected, serializedData);
 	}
 }
