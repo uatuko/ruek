3491aae73f7fec1712ec9386200cdb723c87d956
Uditha Atukorala <ua@nuked.zone>
2023-07-04 09:40:15 +0100

    (service) skeleton endpoints to create and retrieve roles

--- src/service/mappers.cpp
+++ src/service/mappers.cpp
@@ -26,6 +26,24 @@ datastore::Identity map(const gk::v1::CreateIdentityRequest *from) {
 	return identity;
 }
 
+datastore::Role map(const gk::v1::CreateRoleRequest *from) {
+	datastore::Role role({
+		.id   = from->id(),
+		.name = from->name(),
+	});
+
+	if (from->permissions_size() > 0) {
+		datastore::Role::permissions_t perms;
+		for (int i = 0; i < from->permissions_size(); i++) {
+			perms.insert(from->permissions(i));
+		}
+
+		role.permissions(std::move(perms));
+	}
+
+	return role;
+}
+
 void map(const datastore::Collection &from, gk::v1::Collection *to) {
 	to->set_id(from.id());
 	to->set_name(from.name());
@@ -37,6 +55,15 @@ void map(const datastore::Identity &from, gk::v1::Identity *to) {
 
 	if (from.attrs()) {
 		google::protobuf::util::JsonStringToMessage(*from.attrs(), to->mutable_attrs());
+	}
+}
+
+void map(const datastore::Role &from, gk::v1::Role *to) {
+	to->set_id(from.id());
+	to->set_name(from.name());
+
+	for (const auto &perm : from.permissions()) {
+		to->add_permissions(perm);
 	}
 }
 } // namespace service
