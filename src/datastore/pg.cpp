bc19dc12bd7be9c98bc8ed4257e09dad140303c1
Uditha Atukorala <ua@nuked.zone>
2023-07-28 11:26:06 +0100

    (datastore) thread-safe maybe fault tolerant pg connections

--- src/datastore/pg.cpp
+++ src/datastore/pg.cpp
@@ -1,22 +1,43 @@
 #include "pg.h"
 
-static std::shared_ptr<datastore::pg::conn_t> _conn = nullptr;
+#include "err/errors.h"
+
+static datastore::config::pg_t _conf;
+static datastore::pg::conn_t   _conn = nullptr;
 
 namespace datastore {
 namespace pg {
-std::shared_ptr<conn_t> conn() {
-	// FIXME: check if initialised
+conn_t::element_type &connection::conn() const {
+	return *_conn;
+}
+
+conn_t::element_type &connection::reconnect() {
+	_conn = connect();
+	return *_conn;
+}
+
+connection conn() {
+	if (!_conn) {
+		throw err::DatastorePgConnectionUnavailable();
+	}
+
+	static std::timed_mutex mutex;
+	if (!mutex.try_lock_for(_conf.timeout)) {
+		throw err::DatastorePgTimeout();
+	}
+
+	return connection(_conn, connection::lock_t(mutex, std::adopt_lock));
+}
+
+conn_t connect() {
+	// Ref: https://www.postgresql.org/docs/current/libpq-envars.html
+	_conn = std::make_shared<conn_t::element_type>(_conf.opts);
 	return _conn;
 }
 
-result_t exec(std::string_view qry) {
-	nontxn_t tx(*conn());
-	return tx.exec(qry);
-}
-
 void init(const config::pg_t &c) {
-	// Ref: https://www.postgresql.org/docs/current/libpq-envars.html
-	_conn = std::make_shared<conn_t>(c.opts);
+	_conf = c;
+	connect();
 }
 } // namespace pg
 } // namespace datastore
--- src/datastore/pg.h
+++ src/datastore/pg.h
@@ -6,7 +6,7 @@
 
 namespace datastore {
 namespace pg {
-using conn_t   = pqxx::connection;
+using conn_t   = std::shared_ptr<pqxx::connection>;
 using row_t    = pqxx::row;
 using result_t = pqxx::result;
 using nontxn_t = pqxx::nontransaction;
@@ -14,13 +14,41 @@ using nontxn_t = pqxx::nontransaction;
 using fkey_violation_t   = pqxx::foreign_key_violation;
 using unique_violation_t = pqxx::unique_violation;
 
-std::shared_ptr<conn_t> conn();
+class connection {
+public:
+	using lock_t = std::unique_lock<std::timed_mutex>;
 
-result_t exec(std::string_view qry);
+	connection(const conn_t &conn, lock_t &&lock) noexcept : _conn(conn), _lock(std::move(lock)) {}
 
-template <typename... Args> inline result_t exec(std::string_view qry, Args &&...args) {
-	nontxn_t tx(*conn());
-	return tx.exec_params(pqxx::zview(qry), args...);
+	auto exec(std::string_view qry, auto &&...args) {
+		try {
+			return nontxn_exec(qry, std::forward<decltype(args)>(args)...);
+		} catch (const pqxx::broken_connection &e) {
+			// Try to reconnect, if it fails will throw an error
+			reconnect();
+		}
+
+		return nontxn_exec(qry, std::forward<decltype(args)>(args)...);
+	}
+
+private:
+	result_t nontxn_exec(std::string_view qry, auto &&...args) const {
+		nontxn_t tx(conn());
+		return tx.exec_params(pqxx::zview(qry), std::forward<decltype(args)>(args)...);
+	}
+
+	conn_t::element_type &conn() const;
+	conn_t::element_type &reconnect();
+
+	conn_t _conn;
+	lock_t _lock;
+};
+
+connection conn();
+conn_t     connect();
+
+inline auto exec(std::string_view qry, auto &&...args) {
+	return conn().exec(qry, std::forward<decltype(args)>(args)...);
 }
 
 void init(const config::pg_t &c);
