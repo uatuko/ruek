8ed56711ff044bbd98c1f2458450744db1e3a424
Uditha Atukorala <ua@nuked.zone>
2023-07-28 17:08:31 +0100

    tidy-up the use of inline

--- src/err/fixed_string.h
+++ src/err/fixed_string.h
@@ -7,11 +7,11 @@ namespace err {
 template <std::size_t N> struct fixed_string {
 	constexpr fixed_string(const char (&str)[N]) { std::copy_n(str, N, value); }
 
-	inline const char *c_str() const noexcept { return value; }
+	const char *c_str() const noexcept { return value; }
 
-	constexpr inline std::size_t size() const noexcept { return N; }
+	constexpr std::size_t size() const noexcept { return N; }
 
-	inline std::string_view str() const noexcept { return value; }
+	std::string_view str() const noexcept { return value; }
 
 	char value[N];
 };
