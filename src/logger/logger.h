f67b55cfdc9d38201e9e7b05e4dcff81c032706b
Uditha Atukorala <ua@nuked.zone>
2023-07-28 08:01:51 +0100

    log and exit for some unhandled exceptions to help debug issues

--- src/logger/logger.h
+++ src/logger/logger.h
@@ -5,17 +5,27 @@
 namespace logger {
 std::string timestamp();
 
-template <typename... Args> void info(std::string_view source, Args &&...args) {
+template <typename... Args>
+inline void log(std::string_view severity, std::string_view source, Args &&...args) {
 	glz::obj obj{
 		"@timestamp",
 		timestamp(),
 		"severity",
-		"info",
+		severity,
 		"source",
 		source,
 		args...,
 	};
 
 	std::cout << glz::write_json(obj) << std::endl;
+}
+
+void critical(std::string_view source, auto &&...args) {
+	log("critical", source, std::forward<decltype(args)>(args)...);
+	std::exit(EXIT_FAILURE);
+}
+
+inline void info(std::string_view source, auto &&...args) {
+	log("info", source, std::forward<decltype(args)>(args)...);
 }
 } // namespace logger
