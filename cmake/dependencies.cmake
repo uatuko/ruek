ecd1835472eed1e87552668ce56d2bb086247f06
Uditha Atukorala <ua@nuked.zone>
2023-12-24 12:39:19 +0000

    (cmake) tidy-up

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -10,19 +10,19 @@ FetchContent_Declare(fmt
 )
 FetchContent_MakeAvailable(fmt)
 
-# grpcxx
-FetchContent_Declare(grpcxx
-	URL      https://github.com/uatuko/grpcxx/archive/7fdb39cb5662f874cf496ab7ec14a7bbe216b0be.tar.gz
-	URL_HASH SHA256=44a8faf2fc1498736928e60ce282ff4f9c18b26ddfd93138a75f333747844591
-)
-FetchContent_MakeAvailable(grpcxx)
-
 # googleapis
 FetchContent_Declare(googleapis
 	URL      https://github.com/googleapis/googleapis/archive/0e3b813b0d0da539eacbe86b8716feeed00943c5.tar.gz
 	URL_HASH SHA256=44f3b9c73a5df760c4fad3cf0c5cc54732b881f00708308f7635ff75a13dcaa5
 )
 FetchContent_MakeAvailable(googleapis)
+
+# grpcxx
+FetchContent_Declare(grpcxx
+	URL      https://github.com/uatuko/grpcxx/archive/7fdb39cb5662f874cf496ab7ec14a7bbe216b0be.tar.gz
+	URL_HASH SHA256=44a8faf2fc1498736928e60ce282ff4f9c18b26ddfd93138a75f333747844591
+)
+FetchContent_MakeAvailable(grpcxx)
 
 # libpqxx
 FetchContent_Declare(libpqxx
