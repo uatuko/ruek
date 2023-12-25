5664137cfa877e3bf4200520779dd5ab26152f06
Uditha Atukorala <ua@nuked.zone>
2023-12-25 14:51:18 +0000

    (encoding) b32 decoder

--- src/encoding/b32_test.cpp
+++ src/encoding/b32_test.cpp
@@ -4,6 +4,30 @@
 
 using namespace encoding;
 
+TEST(encoding_b32, decode) {
+	{
+		auto actual = b32::decode("91imor3f5gg7erridhi22");
+		EXPECT_EQ("Hello, world!", actual);
+	}
+
+	// Test vectors from https://www.rfc-editor.org/rfc/rfc4648#section-10
+	{
+		EXPECT_EQ("", b32::decode(""));
+		EXPECT_EQ("f", b32::decode("co"));
+		EXPECT_EQ("fo", b32::decode("cpng"));
+		EXPECT_EQ("foo", b32::decode("cpnmu"));
+		EXPECT_EQ("foob", b32::decode("cpnmuog"));
+		EXPECT_EQ("fooba", b32::decode("cpnmuoj1"));
+		EXPECT_EQ("foobar", b32::decode("cpnmuoj1e8"));
+	}
+
+	// Error: invalid chars
+	{
+		EXPECT_EQ("", b32::decode("c+o"));
+		EXPECT_EQ("f", b32::decode("cpn+g"));
+	}
+}
+
 TEST(encoding_b32, encode) {
 	{
 		auto actual = b32::encode("Hello, world!");
