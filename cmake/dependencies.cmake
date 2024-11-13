9f26711a6e61e5cfcd7629bb2f6f5bbd4a329e2b
Uditha Atukorala <ua@nuked.zone>
2024-11-13 19:23:11 +0000

    (cmake) drop googleapis as a dependency

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -10,13 +10,6 @@ FetchContent_Declare(fmt
 )
 FetchContent_MakeAvailable(fmt)
 
-# googleapis
-FetchContent_Declare(googleapis
-	URL      https://github.com/googleapis/googleapis/archive/0e3b813b0d0da539eacbe86b8716feeed00943c5.tar.gz
-	URL_HASH SHA256=44f3b9c73a5df760c4fad3cf0c5cc54732b881f00708308f7635ff75a13dcaa5
-)
-FetchContent_MakeAvailable(googleapis)
-
 # grpcxx
 FetchContent_Declare(grpcxx
 	URL      https://github.com/uatuko/grpcxx/archive/refs/tags/v0.2.0.tar.gz
